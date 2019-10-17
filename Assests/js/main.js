06464469df4f27b895841583d9b33f322eb72453
Hidora <bedouijesser@gmail.com>
2019-10-17 20:26:01 +0100

    getting started

--- Assests/js/main.js
+++ Assests/js/main.js
@@ -1,13 +1,31 @@
+
+// Timer function
+// Display in-game feedback
+// User interface
+// Scorebord 
+
 // getting the userTry from the text input
 const userTry = document.getElementById("userTry") ;
 const errorBox = document.getElementById("errorBox");
 
 
 
-function check(){
+const check = () =>{
     // making sure the user doesn't return an empty
     if (userTry.value ===  ""){
         errorBox.innerHTML = "Please type in your try";
         return false;
     }
 }
+
+
+let secs = 0;
+let mins = 0;
+
+    setInterval (() => {
+        secs ++;
+        formTimer = ("0" + mins).slice(-2) + " : " + ("0" + secs).slice(-2);;
+        console.log( formTimer );
+        if ( secs == 59 )
+        { mins ++ ; secs = -1 }
+    }, 1000 );
