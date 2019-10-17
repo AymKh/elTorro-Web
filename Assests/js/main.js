8ee1af1a62428bc9e4c8938670cdb756e8f79378
Hidora <bedouijesser@gmail.com>
2019-10-18 00:53:58 +0100

    added some js + modified some old ass html m8

--- Assests/js/main.js
+++ Assests/js/main.js
@@ -7,17 +7,19 @@
 // getting the userTry from the text input
 const userTry = document.getElementById("userTry") ;
 const errorBox = document.getElementById("errorBox");
+const testForm = document.querySelector("#form1");
 
 
-
-const check = () =>{
+// form checker
+testForm.addEventListener('submit' , e =>{
+    e.preventDefault();
     // making sure the user doesn't return an empty
-    if (userTry.value ===  ""){
+    if (userTry.value ===  "")
+    {
         errorBox.innerHTML = "Please type in your try";
-        return false;
     }
-    // making sure the user doesn't time a wrong letter
-}
+    // making sure the user doesn't type a wrong letter
+})
 
 
 let secs = 0;
