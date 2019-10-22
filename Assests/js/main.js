897c58fb207a736cd1edc7f38fd03e06990db87e
AymKh <aymenkhedhria@gmx.com>
2019-10-22 12:13:40 -0700

    stopping timer after 10 attempts

--- Assests/js/main.js
+++ Assests/js/main.js
@@ -194,6 +194,7 @@ const timer = document.getElementById("tmr");
                 }else if (triz == 10){
                     table.innerHTML += `<tr class="red"> <td style ="font-weight:bold;">${userTry.value}</td> <td>C : ${testCows}</td> <td>B : ${testBulls}</td> <td>A :${triz}</td></tr>`;
 			userTry.setAttribute("disabled", "");
+			sTimer(0);
                 }else {
                     table.innerHTML += `<tr > <td style ="font-weight:bold;">${userTry.value}</td> <td>C : ${testCows}</td> <td>B : ${testBulls}</td> <td>A :${triz}</td></tr>`;
                 }
